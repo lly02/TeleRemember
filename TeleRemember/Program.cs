72bca676deb483410c5d7ba3094d7ed2a3d3d5af
lly02 <lly.02@outlook.com>
2023-06-25 22:44:45 +0800

    initial commit

---
+++ TeleRemember/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace TeleRemember
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            IHost host = builder.Build();
+            host.Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder()
+        {
+
+        }
+    }
+}
