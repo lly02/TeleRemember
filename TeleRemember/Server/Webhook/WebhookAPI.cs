03a535d7530597507be77488636a32182b357705
lly02 <lly.02@outlook.com>
2023-06-30 17:33:19 +0800

    feat: set up webhook

--- TeleRemember/Server/Webhook/WebhookAPI.cs
+++ TeleRemember/Server/Webhook/WebhookAPI.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Nodes;
@@ -11,9 +13,37 @@ namespace TeleRemember.Server.Webhook
 {
     public class WebhookAPI
     {
-        public static async void Webhook(HttpRequest request)
+        private readonly ILogger _logger;
+
+        public WebhookAPI(
+            ILogger<WebhookAPI> logger)
+        {
+            _logger = logger;
+        }
+
+        public async void Webhook(HttpRequest request)
         {
             JsonNode data = await HttpHelper.HttpRequestToJsonAsync(request);
+            JsonNode chat = data["message"]!["chat"]!;
+            string message = data["message"]!["text"]!.ToString();
+
+            if (chat!["type"]!.ToString() != "group")
+            {
+                _logger.LogInformation("The bot currently only replies in a group.");
+                return;
+            }
+
+            if (!BotHelper.IsCommand(message)) return;
+
+            string? command = BotHelper.GetCommand(message);
+
+            if (command == "")
+            {
+                _logger.LogInformation("Invalid command: {message}", message);
+                return;
+            }
+
+            _logger.LogInformation("Command received: {message}", command);
         }
     }
 }
