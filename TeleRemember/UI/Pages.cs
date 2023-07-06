a744b502ab7ec94ae4947b42c915f3404f0f3456
lly02 <lly.02@outlook.com>
2023-07-06 14:16:41 +0800

    feat: static page for menu

---
+++ TeleRemember/UI/Pages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleRemember.UI
+{
+    public static class Pages
+    {
+        public static string Menu = @"
+*COMMANDS*
+/display \- Display all added schedules
+/add \- Add a new schedule
+        ";
+
+
+    }
+}
